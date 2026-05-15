89c443ed26ef61f2dc7e04b6544a50af82c717be
daylight55 <jojomasanori3@gmail.com>
2026-05-15 09:34:04 +0900

    Deploy production site to Cloudflare Pages

--- infra/cloudflare-pages/main.tf
+++ infra/cloudflare-pages/main.tf
@@ -16,7 +16,7 @@ resource "cloudflare_pages_project" "research" {
       repo_name                      = var.github_repo_name
       repo_id                        = var.github_repo_id
       production_branch              = var.production_branch
-      production_deployments_enabled = true
+      production_deployments_enabled = false
       pr_comments_enabled            = true
       preview_deployment_setting     = "all"
       preview_branch_includes        = var.preview_branch_includes
@@ -25,4 +25,22 @@ resource "cloudflare_pages_project" "research" {
       path_excludes                  = var.path_excludes
     }
   }
+}
+
+resource "cloudflare_pages_domain" "custom_domain" {
+  account_id   = var.account_id
+  project_name = cloudflare_pages_project.research.name
+  name         = var.custom_domain_name
+}
+
+resource "cloudflare_dns_record" "pages_custom_domain" {
+  zone_id = var.custom_domain_zone_id
+  name    = var.custom_domain_name
+  type    = "CNAME"
+  content = cloudflare_pages_project.research.subdomain
+  proxied = true
+  ttl     = 1
+  comment = "Managed by Terraform for Cloudflare Pages project ${cloudflare_pages_project.research.name}."
+
+  depends_on = [cloudflare_pages_domain.custom_domain]
 }

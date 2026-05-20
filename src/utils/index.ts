572bfee144564cbb186545921e5471784eeb6b3b
daylight55 <jojomasanori3@gmail.com>
2026-05-20 13:23:39 +0900

    Add tech news source workflow

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,5 +1,13 @@
 export { sluglify, unsluglify } from './sluglify'
 export { cn } from './cn'
 export { withBase, withoutBase } from './basePath'
-export { getCategories, getPosts, getTags, getPostByTag, filterPostsByCategory } from './post'
+export {
+	getCategories,
+	getPosts,
+	getReportPosts,
+	getNewsPosts,
+	getTags,
+	getPostByTag,
+	filterPostsByCategory
+} from './post'
 export { remarkReadingTime } from './readTime'
